b3f5ae066faaeac31f05b4bed1df642187c98b2b
developer1-2-5 <66413272+developer1-2-5@users.noreply.github.com>
2020-06-05 11:28:32 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+<?php require(__DIR__."/includes/header.php");?>
+
+<body>
+  <div id="page-wrap">
+    <div id="header">
+      <h1><a href="">PHP Sample Test App</a></h1>
+    </div>
+
+    <div id="main">
+      <noscript>This site just doesn't work, period, without JavaScript</noscript>
+
+      <ul id="list" class="ui-sortable">
+      <?php 
+          $del_e="";
+          $del_s="";
+          $alllist = $obj->viewall();
+          $count="";
+          foreach($alllist as $data){
+            
+            $text = $data['text'];
+            $id = $data['id'];
+            $color = $data['color'];
+            $status = $data['status'];
+            $count++;
+          
+        ?>
+        <li color="1" class="<?php echo $color;?>" rel="<?php echo $id;?>" id="<?php echo $id;?>">
+      
+         <?php if($status == "1"){
+           $del_s="<del>";
+           $del_e="</del>";
+         }else{
+          $del_s="";
+          $del_e="";
+         }?>
+         <?php echo $del_s;?><input class="spandetector" id="2listitem" title="Double-click to edit..." style="opacity: 1;" value="<?php echo $text;?>"><?php echo $del_e;?>
+         <!-- <?php echo $del_s;?><span  id="2listitem" title="Double-click to edit..." style="opacity: 1;"><?php echo $text;?></span></del><?php echo $del_e;?> -->
+
+          <div class="draggertab tab" rel="<?php echo $id;?>"></div>
+
+          <div class="colortab tab"></div>
+          <div class="deletetab tab" style="width: 44px; display: block; right: -64px;">
+          </div>
+          <div class="donetab tab"></div>
+        </li>
+          <?php } ?>
+      </ul>
+	  <br />
+
+      <form  id="add-new" method="post">
+        <input type="text" id="new-list-item-text" name="new-list-item-text" />
+        <a href="jacascript:;" id="add-new-submit" value="Add" class="button" >Add</a>
+      </form>
+
+      <div class="clear"></div>
+    </div>
+  </div>
+</body>
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery-ui.min_1.js"></script>
+<script src="js/customjs.js"></script>
+</html>
---
+++ js/customjs.js
@@ -0,0 +1,122 @@
+$(document).ready(function(){
+    $(".buttonsave").hide();
+})
+$("#add-new-submit").click(function(){
+    var check = $("#new-list-item-text").val();
+    if(check ==""){
+        alert("insert data");
+    }else{
+        var form = $("#add-new").serialize();
+        //console.log(form);
+        $.ajax({
+            url: "includes/ajax.php",
+            data: form+"&type=insert",
+            method:"POST",
+            success: function(data) {
+                $("#list").html(data);
+                $("#new-list-item-text").val('');
+            }
+        })
+    }
+})
+
+$(function(){
+    var index = 0;
+    var class_list = ["colorGreen", "colorRed", "colorBlue"];
+    $(document).on("click",".colortab",function() { 
+        $(this).closest('li').removeClass();
+            $(this).closest('li').toggleClass(class_list[index++],class_list[index]);
+             index %= class_list.length;
+             var id = $(this).closest('li').attr('rel');
+             var color =  $(this).closest('li').attr("class");
+             type="updatecolor";    
+            $.ajax({
+                url: "includes/ajax.php",
+                data:{id:id,type:type,color:color},
+                method:"POST",
+                success: function(data) {
+                    $("#list").html(data);
+                    
+                }
+            })
+    });
+});
+$(document).on("click",".deletetab",function() {
+    if (confirm('Are you sure ?')) {
+        // Save it!
+        var id = $(this).closest('li').attr('rel');
+        type="delete";    
+    //console.log(form);
+        $.ajax({
+            url: "includes/ajax.php",
+            data:{id:id,type:type},
+            method:"POST",
+            success: function(data) {
+                $("#list").html(data);
+                
+            }
+        })
+      } else {
+        // Do nothing!
+       
+      }
+})
+$('span').bind('dblclick',function(){
+        $(this).attr('contentEditable',true);
+        $(this).append('<a class="buttonsave">Save</a>');
+});
+//$('span').on('change',function(){
+$(document).on("change",".spandetector",function() {
+    var text = $(this).closest('li').find("input").val();
+    var id = $(this).closest('li').attr('rel');
+ //   var color =  $(this).closest('li').attr("class");
+   // alert(color);
+    type="updatetext";    
+        $.ajax({
+            url: "includes/ajax.php",
+            data:{id:id,type:type,text:text},
+            method:"POST",
+            success: function(data) {
+                $("#list").html(data);
+                $(".buttonsave").hide();
+                //location.reload();
+            }
+        })
+})
+$(document).on("click",".donetab",function() {
+    var id = $(this).closest('li').attr('rel');
+    var type="markread";    
+    $.ajax({
+        url: "includes/ajax.php",
+        data:{id:id,type:type},
+        method:"POST",
+        success: function(data) {
+            $("#list").html(data);
+            $(".buttonsave").hide();
+        }
+    })
+})
+$(document).on("click",".draggertab ",function() {
+var $sortable = $( "#list" );
+$sortable.sortable({
+    stop: function ( event, ui ) {
+        var type = "sorting";
+        var parameters = $sortable.sortable( "toArray" );
+        $.post("includes/ajax.php",{value:parameters,type:type},function(result){
+            $("#list").html(data);
+        });
+    }
+});
+})
+$('input').on({
+    focus: function() {
+        if (!$(this).data('disabled')) this.blur()
+    },
+    dblclick: function() {
+        $(this).data('disabled', true);
+        this.focus()
+    },
+    blur: function() {
+        $(this).data('disabled', false);
+    }
+});
